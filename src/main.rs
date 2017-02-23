954401fd8132e3281c05a5084683249bfc05a3bc
Hajime Fukuda <hajime.fukuda@me.com>
2017-02-24 00:56:34 +0900

    added method call

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ use emval::*;
 
 fn main() {
     let alert = JSObj::global("alert");
-    alert.call(args!("こんにちは、世界！"));
+    alert.call(args!("Hello, world!"));
+
+    /*let window = JSObj::global("window");
+    println!("{:?}", window);
+    let alert = window.get_prop("alert");
+    println!("{:?}", alert);
+    alert.call(args!("Hello, world!"));
+    */
+    let window = JSObj::global("window");
+    window.call_void_prop("alert", args!("Hello, world!"));
+
     println!("Hello, world!");
 }
